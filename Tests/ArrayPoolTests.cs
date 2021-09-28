178bdf62aea3a2f04409dcd3c56b21276ac7753c
electricessence <5899455+electricessence@users.noreply.github.com>
2021-09-27 21:02:02 -0700

    Verified ArrayPool behavior.

---
+++ Tests/ArrayPoolTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Open.Compression.Tests
+{
+	public static class ArrayPoolTests
+	{
+		[Fact]
+		public static void ArrayPoolSizeTest()
+		{
+			var pool = ArrayPool<int>.Shared;
+			const int maxSize = 1024 * 1024;
+
+			var a = pool.Rent(maxSize);
+			Assert.True(a.Length >= maxSize);
+			pool.Return(a);
+
+			var testSize = maxSize + 1;
+			var b = pool.Rent(testSize);
+			Assert.True(b.Length >= maxSize);
+			pool.Return(b);
+
+			testSize = int.MaxValue / 2;
+			var c = pool.Rent(testSize);
+			Assert.True(c.Length >= testSize);
+			pool.Return(c);
+		}
+	}
+}
