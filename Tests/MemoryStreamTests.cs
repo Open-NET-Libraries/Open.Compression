6f255a82b1e86c4994583ec0de5c38330ee5eb14
electricessence <5899455+electricessence@users.noreply.github.com>
2024-04-24 14:02:50 -0700

    Added more tests. Updated to .NET 8.

---
+++ Tests/MemoryStreamTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Open.Compression.Tests;
+public static class MemoryStreamTests
+{
+    [Theory]
+    [InlineData("Hello World.")]
+    [InlineData("The quick brown fox jumped over the lazy dog.")]
+    [InlineData("!@#$%^&*((***&&&*((())){}|}")]
+    [InlineData("<xml>data</xml>")]
+    public static void MemoryStreamRoundTripTest(string value)
+    {
+        var bytes = Encoding.UTF8.GetBytes(value);
+        var compressed = Gzip.Compress(bytes);
+        var decompressed = Gzip.Decompress(compressed);
+        Assert.Equal(bytes, decompressed);        
+    }
+}
